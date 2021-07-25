c57138397a00560890f37b5a4ea37103c6ffb947
champaksworld <champaksworld@users.noreply.github.com>
2021-07-25 18:15:24 +0530

    ListView

--- lib/VsjTwo.dart
+++ lib/VsjTwo.dart
@@ -1,26 +1,46 @@
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-class VsjTwo extends StatefulWidget {
-  @override
-  _VsjTwoState createState() => _VsjTwoState();
-
+class VsjTwo extends StatelessWidget {
+String message="",sender="";
+VsjTwo(String message,String sender)
+{
+  this.message=message;
+  this.sender=sender;
 }
-class _VsjTwoState extends State<VsjTwo> {
+static TextStyle myTextStyle()
+{
+  return TextStyle(color: Colors.white,backgroundColor: Colors.blue,fontSize: 20,);
+}
+static InputDecoration myInputDecoration() {
+ return InputDecoration(hintText: "Text Input", border: OutlineInputBorder());
+}
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("Firebase Auth Demo"),
-              centerTitle: true,
-            ),
-            body: Center(
-                child: Column()
-            )
-        ));
+    return Material(
+        color: Colors.blueAccent,
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(10),
+        child:
+    Row(
+
+                  children: [Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(message,style:myTextStyle(),),
+
+                  ),
+                   Padding(
+                     padding: const EdgeInsets.all(2.0),
+                     child: Text(" : ",style: TextStyle(backgroundColor: Colors.white),),
+                   )
+                    ,Text(sender,style:myTextStyle(),)],
+                )
+    )
+
+        ;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,149 @@
 import 'dart:convert';
 
+import 'package:firebasedemo/VsjTwo.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
-  //Firebase.initializeApp();
+void main() async {
+  while(true)
+    {
+ try {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+   break;
+ }
+ catch(e) {}
+  }
   runApp(FirebaseDemo());
-
 }
 
 class FirebaseDemo extends StatefulWidget {
-  FirebaseFirestore firestoredb;//=FirebaseFirestore.instance;
+  static FirebaseFirestore firestoredb; //=FirebaseFirestore.instance;
   @override
   _FirebaseDemoState createState() => _FirebaseDemoState();
-
 }
 
 class _FirebaseDemoState extends State<FirebaseDemo> {
-
   String username = "", password = "", status = "Messages";
 
   @override
   void initState() {
     super.initState();
-   firebaseInit();
+    firebaseInit();
   }
-void firebaseInit()
-{
-  try {
-    Firebase.initializeApp().whenComplete(() {
-      widget.firestoredb=FirebaseFirestore.instance;
-      print("Initialized");
-    });
-  } catch (ee) {
-    print(ee);
+
+  void firebaseInit() {
+    try {
+      //Firebase.initializeApp().whenComplete(() {
+
+      // print("Initialized");
+      // });
+      FirebaseDemo.firestoredb = FirebaseFirestore.instance;
+    } catch (ee) {
+      print(ee);
+    }
   }
-}
-  _FirebaseDemoState() {
-  //_firestoredb=  FirebaseFirestore.instance;
-  }
-  String firebasedata="data";
-  void getMessages()
- async {
-    FirebaseFirestore _firestoredb=FirebaseFirestore.instance;
-    dynamic result= await  _firestoredb.collection("messages").get();
-    QuerySnapshot messages=result;
+
+  _FirebaseDemoState() {}
+  String firebasedata = "data";
+  void getMessages() async {
+    dynamic result =
+        await FirebaseDemo.firestoredb.collection("messages").get();
+    QuerySnapshot messages = result;
     print(messages);
-    firebasedata="";
+    firebasedata = "";
     for (var message in messages.docs) {
       print(message.data());
-      firebasedata=firebasedata + message.data().toString() + "\n";
+      firebasedata = firebasedata + message.data().toString() + "\n";
       print(message.get("chatmessage"));
       print(message.get("messagefrom"));
-
-    
-     // print(message.data().chatmessage);
     }
     print(firebasedata);
-setState(() {
-
-});
+    setState(() {});
   }
-  void getMessagesStream()async {
+
+  void getMessagesStream() async {
     print("Streams");
-    FirebaseFirestore _firestoredb = FirebaseFirestore.instance;
-    dynamic result = await _firestoredb.collection("messages").snapshots();
+
+    dynamic result =
+        await FirebaseDemo.firestoredb.collection("messages").snapshots();
     print(result.runtimeType);
-    Stream<QuerySnapshot> ms=result;
-    setState(() {
-
-    });
-    firebasedata="";
+    Stream<QuerySnapshot> ms = result;
+    setState(() {});
+    firebasedata = "";
     ms.forEach((element) {
-      for(var value in element.docs)
-
-      {
-
+      for (var value in element.docs) {
         print(value.data());
-        firebasedata=firebasedata + value.data().toString() + "\n";
+        firebasedata = firebasedata + value.data().toString() + "\n";
         print(value.get("chatmessage"));
         print(value.get("messagefrom"));
       }
     });
     print(firebasedata);
-
-    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    //firebaseInit();
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.tealAccent,
         appBar: AppBar(
-          title: Text("Firebase Auth Demo"),
+          title: Text("Firebase Chat"),
+          backgroundColor: Colors.teal,
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(
+
             children: <Widget>[
-
-              Expanded(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                child:Text(firebasedata + "data"),
-                ),
+              StreamBuilder<QuerySnapshot>(
+                stream: FirebaseDemo.firestoredb
+                    .collection("messages")
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  try {
+                    if (snapshot.hasData) {
+                      final messages = snapshot.data.docs;
+                      List<Widget> lst = [];
+                      for (var message in messages) {
+                        final messagetext = message.get("chatmessage");
+                        final sender = message.get("messagefrom");
+                        final messagetextfield = VsjTwo(
+                          messagetext.toString(),
+                              sender.toString(),
+                          );
+                        lst.add(messagetextfield);
+                        lst.add(SizedBox(
+                          height: 10,
+                        ));
+                      }
+                      return Column(
+                        children: lst,
+                      );
+                    } else {
+                      List<Text> lst = [];
+                      lst.add(Text("Waiting"));
+                      return Column(
+                        children: lst,
+                      );
+                    }
+                  } catch (e) {
+                    List<Text> lst = [];
+                    lst.add(Text("Erromr" + e.toString()));
+                    return Column(
+                      children: lst,
+                    );
+                  }
+                },
               ),
-
-
-              Text(
-                status,
-                style: TextStyle(backgroundColor: Colors.teal),
-              )
-              ,
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: TextField(
+                  decoration: VsjTwo.myInputDecoration(),
+
                   textAlign: TextAlign.center,
                   onChanged: (value) {
                     username = value;
@@ -128,126 +154,17 @@ setState(() {
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: TextField(
+
                   textAlign: TextAlign.center,
-                  obscureText: true,
+decoration: VsjTwo.myInputDecoration(),
                   onChanged: (value) {
                     password = value;
                     print(password);
                   },
                 ),
               ),
-              ElevatedButton(
-                  child: Text(
-                    "Register",
-                    style: TextStyle(backgroundColor: Colors.teal),
-                  ),
-                  onPressed: () async {
-                    FirebaseAuth _auth;
-                    try {
-                      print("Register Pressed");
 
-                      try {
-                        _auth = FirebaseAuth.instance;
-
-                      } catch (e1) {
-
-                        String err=e1.errMsg();
-                        status="Creation Failed $err" ;
-                      }
-
-                       dynamic result= await _auth.createUserWithEmailAndPassword(email: username, password: password);
-                      if (result==null)
-                        {
-
-                          status="Creation Failed " ;
-                        }
-                      else
-                        {
-                          UserCredential newcr=result;
-                          User newuser=newcr.user;
-                          String mail=newuser.email;
-                        status="  $mail created";
-
-                        }
-
-                    } catch (e) {
-                      status = e.toString();
-
-                      print(status);
-                    }
-                    setState(() {});
-                  }),
-              ElevatedButton(onPressed: () async{
-print("Send");
-  FirebaseFirestore _firestoredb=FirebaseFirestore.instance;
-                 await     _firestoredb.collection("messages").add(
-                          {
-                            "chatmessage":username,
-                            "messagefrom":"chmpaksworld@gmail.com"
-
-                          });
-              }, child: Text("Send Message Stream",style: TextStyle(backgroundColor: Colors.teal),)),
-             ElevatedButton(onPressed: ()async{
-               print("Get Messages");
-               //await firebaseInit();
-             //await getMessages();
-              await getMessagesStream();
-              
-             }, child:Text("Get Message Stream",style: TextStyle(backgroundColor: Colors.teal),)
-             ),
-
-
-
-
-
-
-
-
-
-              StreamBuilder<QuerySnapshot>(
-                stream:widget.firestoredb.collection("messages").snapshots() ,
-                builder: (context,snapshot){
-                  try {
-                    if (snapshot.hasData) {
-                      final messages = snapshot.data.docs;
-                      List<Text> lst = [];
-                      for (var message in messages) {
-                        final messagetext = message.get("chatmessage");
-                        final sender = message.get("messagefrom");
-                        final messagetextfield = Text(
-                            messagetext.toString() + " : " + sender.toString(),
-                        style: TextStyle(backgroundColor: Colors.pinkAccent),);
-                        lst.add(messagetextfield);
-
-                      }
-                      return Column(
-                        children: lst,
-                      );
-                    }
-                    else {
-                      List<Text> lst = [];
-                      lst.add(Text("Waiting"));
-                      return Column(
-                      children:  lst,
-                      );
-                    }
-                  }
-                  catch(e)
-                  {
-                    List<Text> lst = [];
-                    lst.add(Text("Erromr" + e.toString()));
-                    return Column( children:lst,);
-                  }
-                },
-              ),
-
-
-
-
-
-
-
-
+ElevatedButton(onPressed: (){}, child: Text("Send"))
 
             ],
           ),
